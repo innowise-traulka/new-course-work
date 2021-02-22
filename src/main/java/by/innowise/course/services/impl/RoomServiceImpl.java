b54f1f5c4299aa03a9cf9d01d9bb34b914ebd59d
traulko <traulko03@gmail.com>
2021-02-22 15:39:39 +0300

    added services for each entity

---
+++ src/main/java/by/innowise/course/services/impl/RoomServiceImpl.java
@@ -0,0 +1,6 @@
+package by.innowise.course.services.impl;
+
+import by.innowise.course.services.RoomService;
+
+public class RoomServiceImpl implements RoomService {
+}
---
+++ src/main/java/by/innowise/course/services/impl/UserRoleServiceImpl.java
@@ -0,0 +1,6 @@
+package by.innowise.course.services.impl;
+
+import by.innowise.course.services.UserRoleService;
+
+public class UserRoleServiceImpl implements UserRoleService {
+}
